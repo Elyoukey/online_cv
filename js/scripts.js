eb1c3f9c44f4063495d7237c47a2f4d6ea13462b
lucas lebielle <lucas@lebielle.com>
2020-04-24 00:45:22 +0000

    Update 04 2020

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,48 @@
-window.addEventListener('scroll', function(e){
+window.onload = function(){
+	/*Language selection*/
+	var availableLang =  ['fr','en'];
+	var lang=new URL(location.href).searchParams.get('lang');
+	lang = ( availableLang.indexOf(lang) == -1 )?'en':lang;
+	
+	var elts = document.querySelectorAll('[lang]');
+	for( var i=0; i<elts.length;i++){
+		if(elts[i].lang !== lang ) elts[i].style.display='none';
+	}
+	
+	/*Scroll effect*/
+	window.addEventListener('scroll', function(e){
 		var sTop = document.documentElement.scrollTop ;
 		
 		if(sTop>20){
-			var h = document.querySelectorAll('h1');
-			h[0].className='scrolled';
-			console.log(h[0]);
+			var h = document.querySelectorAll('h1, #hamburger-menu');
+			for( var i=0;i<h.length;i++){
+				h[i].className='scrolled';
+			}
 		}else{
-			var h = document.querySelectorAll('h1');
-			h[0].className='notscrolled';
+			var h = document.querySelectorAll('h1, #hamburger-menu');
+			for( var i=0;i<h.length;i++){
+				h[i].className='notscrolled';
+			}
 		}
 		
 	});
+	
+	var button = document.querySelectorAll('#hamburger-menu');
+	button[0].onclick = function(){
+		var menu = document.querySelectorAll('#left-menu')[0];
+		if( menu.className !== "scrolledshown" ){
+			menu.className =   "scrolledshown";
+			this.className = "scrolled close"
+		}
+		else{
+		menu.className =   "";
+			this.className = "scrolled";
+		}
+	}
+	
+	var menu = document.querySelectorAll('#left-menu ul');
+	menu[0].onclick = function(){
+		var tohidemenu = document.querySelectorAll('#left-menu.scrolledshown')[0];
+		tohidemenu .className = "";
+	}
+}
